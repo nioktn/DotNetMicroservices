b9f6d5013db0cd7e2a3ebf7892c59a5146190988
Nikita Nikitin <>
2021-10-16 00:12:53 +0300

    Added Dtos for Read and Create publisher model

---
+++ Dtos/PlatformReadDto.cs
@@ -0,0 +1,10 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformReadDtos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
